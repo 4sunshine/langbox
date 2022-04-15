a22961a3aace013023ece4190e2ea228e16ceb82
4sunshine <alexandershain.5@gmail.com>
2022-04-16 00:29:18 +0300

    Rudalle added

---
+++ run_rudalle.py
@@ -0,0 +1,118 @@
+"""CODE BASED ON: https://github.com/ai-forever/ru-dalle"""
+
+import multiprocessing
+import os
+
+import torch
+from psutil import virtual_memory
+import transformers
+import more_itertools
+from tqdm.auto import tqdm
+
+from rudalle.pipelines import show, cherry_pick_by_clip
+from rudalle import get_rudalle_model, get_tokenizer, get_vae, get_ruclip
+from rudalle.utils import seed_everything, torch_tensors_to_pil_list
+
+
+def memory_check(allowed_memory=7.5):
+    total_memory = torch.cuda.get_device_properties(0).total_memory / 2**30
+    if total_memory < allowed_memory:
+        raise MemoryError
+    print('Total GPU RAM:', round(total_memory, 2), 'Gb')
+
+    ram_gb = round(virtual_memory().total / 1024**3, 1)
+    print('CPU:', multiprocessing.cpu_count())
+    print('RAM GB:', ram_gb)
+    print("PyTorch version:", torch.__version__)
+    print("CUDA version:", torch.version.cuda)
+    print("cuDNN version:", torch.backends.cudnn.version())
+
+    if torch.__version__ >= '1.8.0':
+        k = allowed_memory / total_memory
+        torch.cuda.set_per_process_memory_fraction(k, 0)
+        print('Allowed GPU RAM:', round(allowed_memory, 2), 'Gb')
+        print('GPU part', round(k, 4))
+
+
+def get_models():
+    # prepare models:
+    device = 'cuda'
+    dalle = get_rudalle_model('Malevich', pretrained=True, fp16=True, device=device)
+    tokenizer = get_tokenizer()
+    vae = get_vae(dwt=True)
+    # prepare utils:
+    ruclip, ruclip_processor = get_ruclip('ruclip-vit-base-patch32-v5')
+    return dalle, tokenizer, vae, ruclip, ruclip_processor
+
+def generate_codebooks(text, tokenizer, dalle, top_k, top_p, images_num, image_prompts=None, temperature=1.0, bs=8,
+                    seed=None, use_cache=True):
+    vocab_size = dalle.get_param('vocab_size')
+    text_seq_length = dalle.get_param('text_seq_length')
+    image_seq_length = dalle.get_param('image_seq_length')
+    total_seq_length = dalle.get_param('total_seq_length')
+    device = dalle.get_param('device')
+    text = text.lower().strip()
+    input_ids = tokenizer.encode_text(text, text_seq_length=text_seq_length)
+    codebooks = []
+    for chunk in more_itertools.chunked(range(images_num), bs):
+        chunk_bs = len(chunk)
+        with torch.no_grad():
+            attention_mask = torch.tril(torch.ones((chunk_bs, 1, total_seq_length, total_seq_length), device=device))
+            out = input_ids.unsqueeze(0).repeat(chunk_bs, 1).to(device)
+            has_cache = False
+            if image_prompts is not None:
+                prompts_idx, prompts = image_prompts.image_prompts_idx, image_prompts.image_prompts
+                prompts = prompts.repeat(chunk_bs, 1)
+            for idx in tqdm(range(out.shape[1], total_seq_length)):
+                idx -= text_seq_length
+                if image_prompts is not None and idx in prompts_idx:
+                    out = torch.cat((out, prompts[:, idx].unsqueeze(1)), dim=-1)
+                else:
+                    logits, has_cache = dalle(out, attention_mask,
+                                              has_cache=has_cache, use_cache=use_cache, return_loss=False)
+                    logits = logits[:, -1, vocab_size:]
+                    logits /= temperature
+                    filtered_logits = transformers.top_k_top_p_filtering(logits, top_k=top_k, top_p=top_p)
+                    probs = torch.nn.functional.softmax(filtered_logits, dim=-1)
+                    sample = torch.multinomial(probs, 1)
+                    out = torch.cat((out, sample), dim=-1)
+            codebooks.append(out[:, -image_seq_length:].cpu())
+    return codebooks
+
+
+def prepare_codebooks(text, tokenizer, dalle, dalle_bs=4, seed=42):
+    seed_everything(seed)
+    codebooks = []
+    for top_k, top_p, images_num in [
+        (2048, 0.995, 8),
+        (1536, 0.99, 8),
+        (1024, 0.99, 8),
+        (1024, 0.98, 8),
+        (512, 0.97, 8),
+        (384, 0.96, 8),
+        (256, 0.95, 8),
+        (128, 0.95, 8),
+    ]:
+        codebooks += generate_codebooks(text, tokenizer, dalle, top_k=top_k, images_num=images_num, top_p=top_p,
+                                        bs=dalle_bs)
+    return codebooks
+
+def synth_images(codebooks, out_dir = 'images_output'):
+    pil_images = []
+    os.makedirs(out_dir, exist_ok=True)
+    for _codebooks in tqdm(torch.cat(codebooks).cpu()):
+        with torch.no_grad():
+            images = vae.decode(_codebooks.unsqueeze(0))
+            pil_images += torch_tensors_to_pil_list(images)
+    for i, img in enumerate(pil_images):
+        img.save(os.path.join(out_dir, f'img_{i:02d}.png'))
+    return pil_images
+
+
+if __name__ == '__main__':
+    memory_check()
+    text = 'Проавпр влпоыпоып пвпыооцпкпо и аолаво'
+    models = get_models()
+    dalle, tokenizer, vae, ruclip, ruclip_processor = models
+    codebooks = prepare_codebooks(text, tokenizer, dalle)
+    images = synth_images(codebooks, 'mad_images')
